a130d3752f4c0905c8af4ffd8cdc88c7a272cc78
aWexisWen <iand170401@gmail.com>
2023-06-11 12:41:14 +0800

    update
    first commit

---
+++ includes/footer.php
@@ -0,0 +1,4 @@
+<footer class="footer">
+    <div class="container text-center"><span class="text-muted"><b>Copyright&copy;Hooked | All Rights Reserved | Contact Us: +6017 600 6580</b></span></div>
+</footer>
+    
